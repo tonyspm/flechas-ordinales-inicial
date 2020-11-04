81ccf6fc9ea713ff01395aa5ba01594839a19c29
tonyspm <73495998+tonyspm@users.noreply.github.com>
2020-11-03 19:18:58 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+basic.forever(function () {
+    while (input.buttonIsPressed(Button.A)) {
+        basic.showArrow(ArrowNames.North)
+        basic.showArrow(ArrowNames.NorthEast)
+        basic.showArrow(ArrowNames.East)
+        basic.showArrow(ArrowNames.SouthEast)
+        basic.showArrow(ArrowNames.South)
+        basic.showArrow(ArrowNames.SouthWest)
+        basic.showArrow(ArrowNames.West)
+        basic.showArrow(ArrowNames.NorthWest)
+    }
+})
